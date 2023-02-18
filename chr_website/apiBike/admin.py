6df7d22f5fdcdccb00b1ebb0c5098106bafb3e94
DevFzn <devfzn@gmail.com>
2023-02-18 09:50:10 -0300

    modelos añadidos a Django-admin

--- chr_website/apiBike/admin.py
+++ chr_website/apiBike/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Network, Location, Station, Extra
 
-# Register your models here.
+admin.site.register(Network)
+admin.site.register(Location)
+admin.site.register(Station)
+admin.site.register(Extra)
