38adab8bec93adc5391df5d4066dce478b54ee95
DevFzn <devfzn@gmail.com>
2023-02-19 19:46:38 -0300

    Uso de session con requests, ahora hay datos

--- chr_website/servEvalAmbient/webscrap/get_sea_data.py
+++ chr_website/servEvalAmbient/webscrap/get_sea_data.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+DEVEL = True
 URL_BASE = "https://seia.sea.gob.cl/busqueda/buscarProyectoAction.php"
+session = requests.Session()
 
 def get_info_resultados():
-    info_sea = requests.get(URL_BASE)
+    """
+    Obtiene y retorna cantidad de resultados, crea session válida
+    para consultas posteriores.
+    """
+    info_sea = session.get(URL_BASE)
     soup = bs(info_sea.content, "html.parser")
     resultado = soup.find('div', {'id':"info_resultado"})
 
@@ -16,27 +22,27 @@ def get_info_resultados():
     cantidad_paginas = info[3].split(':')[1].strip()
     cantidad_paginas = int(cantidad_paginas.replace(',',''))
     return cantidad_paginas
-    #print(f"Proyectos: {cantidad_proyectos}, Paginas: {cantidad_paginas}")
 
 
 def get_data_tables(url):
-    datos_sea = requests.get(url)
+    """
+    Retorna el cuerpo de la tabla, requiere de una sesión válida
+    para su correcto funcionamiento
+    """
+    datos_sea = session.get(url)
     soup = bs(datos_sea.content, "html.parser")
-    tabla = soup.find('table', class_="tabla_datos")
-    #tabla = soup.findAll('tbody')
-    #tabla = soup.find('table', {'class':"tabla_datos"})
-    #tabla = soup.find('table', {'class':"tabla_datos", 'summary':"datos"})
-    print(tabla)
+    tabla = soup.find('tbody')
+    return tabla
 
 
-# DEV
-#cantidad_paginas = get_info_resultados()
-cantidad_paginas = 1
+cantidad_paginas = get_info_resultados()
+if DEVEL:
+    cantidad_paginas = 5
 
 for pagina in range(cantidad_paginas):
     indice = pagina + 1
     URL_compaginada = f"{URL_BASE}?_paginador_refresh=1&_paginador_fila_actual={indice}"
-    print(URL_compaginada)
-    get_data_tables(URL_compaginada)
+    table_data = get_data_tables(URL_compaginada)
+    print(table_data)
 
 
